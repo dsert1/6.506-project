fc6617523fe4ae25f454dfc9a70e00fe9d493459
Deniz Sert <dsert@mit.edu>
2023-04-08 12:50:36 -0400

    added tombstone querying first draft

--- quotient_filter_graveyard_hashing/quotient_filter_graveyard_hashing.cpp
+++ quotient_filter_graveyard_hashing/quotient_filter_graveyard_hashing.cpp
@@ -36,5 +36,41 @@ void QuotientFilterGraveyard::advanceToNextBucket(int * b) {
 }
 
 bool QuotientFilterGraveyard::query(int value) {
+    // figure out value fingerprint
+    FingerprintPair f = fingerprintQuotient(value);
+    uint64_t quotient = f.fq;
+    uint64_t remainder = f.fr;
 
+    // compute hash values for the three possible positions of the element
+    uint64_t h1 = hashFunction(value);
+    uint64_t h2 = h1 ^ hashFunction(quotient);
+    uint64_t h3 = h2 ^ hashFunction(remainder);
+
+    // check if any of the three positions contains the element
+    int s = h1 % this->table_size;
+    if (table[s].is_occupied && table[s].value == value) {
+        return true;
+    }
+
+    s = h2 % this->table_size;
+    if (table[s].is_occupied && table[s].value == value) {
+        return true;
+    }
+
+    s = h3 % this->table_size;
+    if (table[s].is_occupied && table[s].value == value) {
+        return true;
+    }
+
+    // check if the element is in a graveyard slot
+    int b = h1 % this->table_size;
+    while (table[b].is_continuation) {
+    if (fingerprintQuotient(value).fq == table[b].value) {
+        return true;
+    }
+    b = (b + 1) % this->table_size;
+}
+
+    // element isnt present in the filter
+    return false;
 }
