fcdbd39cd2135df2df54e48acc994b799417e7ea
Temi Taylor <temit@mit.edu>
2023-04-27 01:08:48 -0400

    finishing normal filter tests

--- tests/normal_filter_tests.cc
+++ tests/normal_filter_tests.cc
@@ -110,6 +110,7 @@ TEST_F(QuotientFilterTest, SingleElementInsert) {
   EXPECT_FALSE(qf->query(qfv(test_bucket, test_remainder + 1)));
   Mdt slot_tests[] = {{test_bucket, true, false, false, test_remainder}};
   check_slots(qf, 1, slot_tests);
+  EXPECT_EQ(qf->size, 1);
 }
 
 // Testing deleting elements in a filter with a single element
@@ -122,6 +123,7 @@ TEST_F(QuotientFilterTest, SingleElementDelete) {
 
   EXPECT_FALSE(qf->query(qfv(test_bucket, test_remainder)));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 
@@ -145,6 +147,7 @@ TEST_F(QuotientFilterTest, TwoSeparateElementsInsert) {
   Mdt slot_tests[] = {{first_bucket, true, false, false, first_remainder},
                       {second_bucket, true, false, false, second_remainder}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 }
 
 // Testing deleting two separated elements
@@ -160,11 +163,13 @@ TEST_F(QuotientFilterTest, TwoSeparateElementsDelete) {
   qf->deleteElement(qfv(second_bucket, second_remainder));
   EXPECT_TRUE(qf->query(qfv(first_bucket, first_remainder)));
   EXPECT_FALSE(qf->query(qfv(second_bucket, second_remainder)));
+  EXPECT_EQ(qf->size, 1);
 
   qf->deleteElement(qfv(first_bucket, first_remainder));
   EXPECT_FALSE(qf->query(qfv(first_bucket, first_remainder)));
   EXPECT_FALSE(qf->query(qfv(second_bucket, second_remainder)));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 // Testing inserting two adjacent elements
@@ -185,6 +190,7 @@ TEST_F(QuotientFilterTest, TwoAdjacentElementsInsert) {
   Mdt slot_tests[] = {{first_bucket, true, false, false, first_remainder},
                       {second_bucket, true, false, false, second_remainder}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 }
 
 // Testing deleting two adjacent elements
@@ -200,11 +206,13 @@ TEST_F(QuotientFilterTest, TwoAdjacentElementsDelete) {
   qf->deleteElement(qfv(first_bucket, first_remainder));
   EXPECT_FALSE(qf->query(qfv(first_bucket, first_remainder)));
   EXPECT_TRUE(qf->query(qfv(second_bucket, second_remainder)));
+  EXPECT_EQ(qf->size, 1);
 
   qf->deleteElement(qfv(second_bucket, second_remainder));
   EXPECT_FALSE(qf->query(qfv(first_bucket, first_remainder)));
   EXPECT_FALSE(qf->query(qfv(second_bucket, second_remainder)));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 
@@ -234,6 +242,7 @@ TEST_F(QuotientFilterTest, SingleRunInsert) {
                       {rb2, false, true, true, remainders[1]},
                       {rb3, false, true, true, remainders[2]}};
   check_slots(qf, 3, slot_tests);
+  EXPECT_EQ(qf->size, 3);
 }
 
 // Deleting elements from a run in the order they were inserted
@@ -257,12 +266,14 @@ TEST_F(QuotientFilterTest, SingleRunDelete) {
   Mdt slot_tests[] = {{bucket, true, false, false, remainders[1]},
                       {rb2, false, true, true, remainders[2]}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 
   qf->deleteElement(qfv(bucket, remainders[1]));
   qf->deleteElement(qfv(bucket, remainders[2]));
   EXPECT_FALSE(qf->query(qfv(bucket, remainders[1])));
   EXPECT_FALSE(qf->query(qfv(bucket, remainders[2])));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 // Deleting elements from a run opposite the order they were inserted
@@ -286,12 +297,14 @@ TEST_F(QuotientFilterTest, SingleRunDeleteReverse) {
   Mdt slot_tests[] = {{bucket, true, false, false, remainders[0]},
                       {rb2, false, true, true, remainders[1]}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 
   qf->deleteElement(qfv(bucket, remainders[1]));
   qf->deleteElement(qfv(bucket, remainders[0]));
   EXPECT_FALSE(qf->query(qfv(bucket, remainders[0])));
   EXPECT_FALSE(qf->query(qfv(bucket, remainders[1])));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 // Testing a run that wraps around the array
@@ -310,6 +323,7 @@ TEST_F(QuotientFilterTest, SingleRunWrapAround) {
                       {0, false, true, true, remainders[2]},
                       {1, false, true, true, remainders[3]}};
   check_slots(qf, 4, slot_tests);
+  EXPECT_EQ(qf->size, 4);
 
   qf->deleteElement(qfv(14, remainders[1]));
   qf->deleteElement(qfv(14, remainders[3]));
@@ -317,12 +331,14 @@ TEST_F(QuotientFilterTest, SingleRunWrapAround) {
   EXPECT_FALSE(qf->query(qfv(14, remainders[1])));
   EXPECT_TRUE(qf->query(qfv(14, remainders[2])));
   EXPECT_FALSE(qf->query(qfv(14, remainders[3])));
+  EXPECT_EQ(qf->size, 2);
 
   qf->deleteElement(qfv(14, remainders[0]));
   qf->deleteElement(qfv(14, remainders[2]));
   EXPECT_FALSE(qf->query(qfv(14, remainders[0])));
   EXPECT_FALSE(qf->query(qfv(14, remainders[2])));
   assert_empty_buckets(qf, 0, NULL);
+  EXPECT_EQ(qf->size, 0);
 }
 
 
@@ -355,6 +371,7 @@ TEST_F(QuotientFilterTest, DoubleRunInsert) {
                       {rb3, false, true, false, remainders[2]},
                       {rb4, false, true, true, remainders[3]}};
   check_slots(qf, 4, slot_tests);
+  EXPECT_EQ(qf->size, 4);
 }
 
 // Inserting elements to create cluster of two runs in reverse order
@@ -382,6 +399,7 @@ TEST_F(QuotientFilterTest, DoubleRunInsertReverse) {
                       {rb3, false, true, false, remainders[0]},
                       {rb4, false, true, true, remainders[1]}};
   check_slots(qf, 4, slot_tests);
+  EXPECT_EQ(qf->size, 4);
 }
 
 // Inserting elements to create cluster of two runs in an interleaved order
@@ -404,6 +422,7 @@ TEST_F(QuotientFilterTest, DoubleRunInsertInterleaved) {
                       {0, false, true, false, remainders[0]},
                       {1, false, true, true, remainders[3]}};
   check_slots(qf, 4, slot_tests);
+  EXPECT_EQ(qf->size, 4);
 }
 
 // Deleting cluster of two runs, starting with the first run
@@ -433,6 +452,7 @@ TEST_F(QuotientFilterTest, DoubleRunDelete) {
   Mdt slot_tests[] = {{rb2, true, false, false, remainders[2]},
                       {rb3, false, true, true, remainders[3]}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 }
 
 // Deleting cluster of two runs, starting with the second run
@@ -449,11 +469,8 @@ TEST_F(QuotientFilterTest, DoubleRunDeleteReverse) {
     EXPECT_TRUE(qf->query(qfv(buckets[i], remainders[i])));
   }
 
-  scan_table(qf);
   qf->deleteElement(qfv(buckets[2], remainders[2]));
-  scan_table(qf);
   qf->deleteElement(qfv(buckets[3], remainders[3]));
-  scan_table(qf);
   EXPECT_TRUE(qf->query(qfv(buckets[0], remainders[0])));
   EXPECT_TRUE(qf->query(qfv(buckets[1], remainders[1])));
   EXPECT_FALSE(qf->query(qfv(buckets[2], remainders[2])));
@@ -464,6 +481,7 @@ TEST_F(QuotientFilterTest, DoubleRunDeleteReverse) {
   Mdt slot_tests[] = {{first_bucket, true, false, false, remainders[0]},
                       {rb2, false, true, true, remainders[1]}};
   check_slots(qf, 2, slot_tests);
+  EXPECT_EQ(qf->size, 2);
 }
 
 // General test with longer runs
@@ -481,6 +499,7 @@ TEST_F(QuotientFilterTest, DoubleRunLonger) {
     qf->insertElement(qfv(buckets[i], remainders[i]));
     b[i] = (first_bucket + i) % 16;
   }
+  EXPECT_EQ(qf->size, 10);
 
   for (int i = 0; i < 10; i ++) {
     EXPECT_TRUE(qf->query(qfv(buckets[i], remainders[i])));
@@ -515,6 +534,105 @@ TEST_F(QuotientFilterTest, DoubleRunLonger) {
                       {b[5], false, true, true, remainders[6]},
                       {b[6], false, true, true, remainders[8]}};
   check_slots(qf, 7, slot_tests2);
+  EXPECT_EQ(qf->size, 7);
 }
 
-// TODO: include interleaving, size tests
+// Testing with runs overlapping a cluster
+TEST_F(QuotientFilterTest, TestInterruptionSimple) {
+  int first_bucket = 3;
+  int remainders[] = {103, 194, 128, 349, 301, 392};
+  int b_remainder = 222;
+  int d_remainder = 444;
+
+  int ba = first_bucket;
+  int bb = (first_bucket + 1) % 16;
+  int bc = (first_bucket + 2) % 16;
+  int bd = (first_bucket + 3) % 16;
+  int buckets[] = {ba, ba, ba, bc, bc, bc};
+
+  int b[8];
+  for (int i = 0; i < 8; i ++) {
+    if (i < 6) {
+      qf->insertElement(qfv(buckets[i], remainders[i]));
+    }
+    b[i] = (first_bucket + i) % 16;
+  }
+  EXPECT_EQ(qf->size, 6);
+
+  qf->insertElement(qfv(bb, b_remainder));
+  EXPECT_TRUE(qf->query(qfv(bb, b_remainder)));
+  qf->insertElement(qfv(bd, d_remainder));
+  EXPECT_TRUE(qf->query(qfv(bd, d_remainder)));
+
+  Mdt slot_tests1[] = {{b[0], true, false, false, remainders[0]},
+                       {b[1], true, true, true, remainders[1]},
+                       {b[2], true, true, true, remainders[2]},
+                       {b[3], true, true, false, b_remainder},
+                       {b[4], false, true, false, remainders[3]},
+                       {b[5], false, true, true, remainders[4]},
+                       {b[6], false, true, true, remainders[5]},
+                       {b[7], false, true, false, d_remainder}};
+  check_slots(qf, 8, slot_tests1);
+  EXPECT_EQ(qf->size, 8);
+
+  for (int i = 0; i < 6; i ++) {
+    qf->deleteElement(qfv(buckets[i], remainders[i]));
+  }
+  EXPECT_TRUE(qf->query(qfv(bb, b_remainder)));
+  EXPECT_TRUE(qf->query(qfv(bd, d_remainder)));
+  Mdt slot_tests2[] = {{bb, true, false, false, b_remainder},
+                       {bd, true, false, false, d_remainder}};
+  check_slots(qf, 2, slot_tests2);
+  EXPECT_EQ(qf->size, 2);
+}
+
+// Testing the invariant-threatening example
+TEST_F(QuotientFilterTest, TestInterruptionTricky) {
+  int first_bucket = 12;
+  int remainders[] = {103, 194, 128, 192, 349, 301, 392};
+  int b_remainder = 222;
+  int d_remainder = 444;
+
+  int ba = first_bucket;
+  int bb = (first_bucket + 1) % 16;
+  int bc = (first_bucket + 2) % 16;
+  int bd = (first_bucket + 3) % 16;
+  int buckets[] = {ba, ba, ba, ba, bc, bc, bc};
+
+  int b[9];
+  for (int i = 0; i < 9; i ++) {
+    if (i < 7) {
+      qf->insertElement(qfv(buckets[i], remainders[i]));
+    }
+    b[i] = (first_bucket + i) % 16;
+  }
+  EXPECT_EQ(qf->size, 7);
+
+  qf->insertElement(qfv(bd, d_remainder));
+  EXPECT_TRUE(qf->query(qfv(bd, d_remainder)));
+  qf->insertElement(qfv(bb, b_remainder));
+  EXPECT_TRUE(qf->query(qfv(bb, b_remainder)));
+
+  Mdt slot_tests1[] = {{b[0], true, false, false, remainders[0]},
+                       {b[1], true, true, true, remainders[1]},
+                       {b[2], true, true, true, remainders[2]},
+                       {b[3], true, true, true, remainders[3]},
+                       {b[4], false, true, false, b_remainder},
+                       {b[5], false, true, false, remainders[4]},
+                       {b[6], false, true, true, remainders[5]},
+                       {b[7], false, true, true, remainders[6]},
+                       {b[8], false, true, false, d_remainder}};
+  check_slots(qf, 9, slot_tests1);
+  EXPECT_EQ(qf->size, 9);
+
+
+  for (int i = 0; i < 7; i ++) {
+    qf->deleteElement(qfv(buckets[i], remainders[i]));
+  }
+  EXPECT_TRUE(qf->query(qfv(bb, b_remainder)));
+  EXPECT_TRUE(qf->query(qfv(bd, d_remainder)));
+  Mdt slot_tests2[] = {{bb, true, false, false, b_remainder},
+                       {bd, true, false, false, d_remainder}};
+  check_slots(qf, 2, slot_tests2);
+  EXPECT_EQ(qf->size, 2);
+}
